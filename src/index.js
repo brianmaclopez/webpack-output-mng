87969ed04267fd70518072340612d81c284017a2
Lugh_Lámfada <healingphases@gmail.com>
2022-10-02 12:55:30 -0700

    stylesheet asset and image asset added to webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Pic from './pic.jpg';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // Adding image to current div
+    const myPic = new Image();
+    myPic.src = Pic;
+
+    element.appendChild(myPic);
   
     return element;
   }
